6f659fa5491d0699c50d75082b24e58203a73313
perevedniuk <perevedniuk@gmail.com>
2019-02-28 12:52:29 +0200

    optimize

--- webpack.config.js
+++ webpack.config.js
@@ -4,55 +4,61 @@ const webpack = require("webpack");
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+module.exports = (env, arg) => {
+    const isDevMode = arg.mode==='development';
 
-module.exports = {
-    entry: [
-        './src/index.tsx',
-    ],
-    output: {
-        path: path.join(__dirname, "/build"),
-        filename: 'bundle.js'
-    },
-    devtool: "source-map",
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json", ".scss"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                loader: 'ts-loader',
-                exclude: path.join(__dirname, '/node_modules/'),
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: "[name]_[local]_[hash:base64]",
+    return {
+        entry: [
+            './src/index.tsx',
+        ],
+        output: {
+            path: path.join(__dirname, "/build"),
+            filename: 'bundle.js'
+        },
+        devtool: isDevMode ? "source-map" : false,
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".json", ".scss"]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    loader: 'ts-loader',
+                    exclude: path.join(__dirname, '/node_modules/'),
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: "style-loader"
+                        },
+                        {
+                            loader: "css-loader",
+                            options: {
+                                modules: true,
+                                importLoaders: 1,
+                                localIdentName: "[name]_[local]_[hash:base64]",
+                            }
                         }
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                include: path.join(__dirname, '/src/'),
-                loaders: ["style-loader", "css-loader", "sass-loader"]
-            }
+                    ]
+                },
+                {
+                    test: /\.scss$/,
+                    include: path.join(__dirname, '/src/'),
+                    loaders: ["style-loader", "css-loader", "sass-loader"]
+                }
+            ]
+        },
+        optimization: {
+            minimize: !isDevMode,
+        },
+        plugins: [
+            new htmlWebpackPlugin({
+                template: "./src/index.html"
+            }),
+            new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /ru/),
+            new extractTextPlugin('style.css'),
+            new BundleAnalyzerPlugin()
         ]
-    },
-    plugins: [
-        new htmlWebpackPlugin({
-            template: "./src/index.html"
-        }),
-        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /ru/),
-        new extractTextPlugin('style.css'),
-        new BundleAnalyzerPlugin()
-    ]
+    }
 };
